e51a1c26a60236c5587650e5e09d284c1fed6371
Aman Rusia <aman@suprath.com>
2019-03-18 18:20:17 +0530

    Removed trainnig of bert for dropout

--- run_classifier.py
+++ run_classifier.py
@@ -652,7 +652,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
 
     (total_loss, per_example_loss, logits, probabilities) = create_model(
         bert_config, is_training, input_ids, input_mask, segment_ids, label_ids,
-        num_labels, use_one_hot_embeddings,flags.train_only_outer_layer)
+        num_labels, use_one_hot_embeddings,params.get('train_only_outer_layer'))
 
     
     tvars = tf.trainable_variables()
